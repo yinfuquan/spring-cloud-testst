6a2fd01940c2a575333c5fb3587be33b5b38315e
yinfuquan <yinfuquan@countrygarden.com.cnn>
2019-11-05 16:24:29 +0800

    gateway

---
+++ spring-cloud/spring-cloud-basic/spring-cloud-gateway/src/main/java/com/yin/GatewayApplication.java
@@ -0,0 +1,22 @@
+package com.yin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author ：yfq
+ * @date ：Created in 2019/11/5 15:23
+ * @description：
+ * @modified By：
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
