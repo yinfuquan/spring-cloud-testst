60ccb224f1f4c058a13134c644bb4f742cc8b83b
yinfuquan <yinfuquan@countrygarden.com.cnn>
2019-11-08 20:07:18 +0800

    fastdfs

---
+++ spring-cloud/spring-cloud-basic/spring-cloud-fastdfs/src/main/java/com/yin/DemFastdfsApplication.java
@@ -0,0 +1,13 @@
+package com.yin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemFastdfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemFastdfsApplication.class, args);
+    }
+
+}
